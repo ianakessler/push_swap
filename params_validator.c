7c457deb3d1f1095cb85bb5961624274b9c2992c
ian kessler <103191330+ianakessler@users.noreply.github.com>
2025-10-13 12:41:31 +0000

    a

--- params_validator.c
+++ params_validator.c
@@ -6,7 +6,7 @@
 /*   By: iaratang <iaratang@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 17:09:58 by iaratang          #+#    #+#             */
-/*   Updated: 2025/10/10 17:24:04 by iaratang         ###   ########.fr       */
+/*   Updated: 2025/10/13 12:41:10 by iaratang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,29 @@
 int is_all_num(int argc, char **argv)
 {
     int i;
+    int j;
 
     i = 1;
     while (i < argc)
     {
-        if (!ft_isdigit(argv[i][0]))
+        j = 0;
+        while (argv[i][j])
         {
-            write(1, "Error\n", 6);
-            return (0);
+            if (ft_isdigit(argv[i][j]))
+                j++;
+            else if (argv[i][j] == '-')
+                j++;
+            else 
+            {
+                write(1, "Error\n", 7);
+                return (0);
+            }
         }
         i++;
     }
     write(1, "All params validated\n", 22);
     return (1);
 }
+
+
 
